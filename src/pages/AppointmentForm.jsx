6f24bcff27b16fac4b52c35842b8edd1eda6ea13
Mehul <mehul.v1990@gmail.com>
2025-05-23 18:50:52 +0530

    fixed UI breaking issue
    fixed UI breaking issue

--- src/pages/AppointmentForm.jsx
+++ src/pages/AppointmentForm.jsx
@@ -33,7 +33,7 @@ export default function AppointmentForm() {
                     ...appointment,
                     start_time: appointment.startTime,
                     end_time: appointment.endTime,
-                    participants: appointment.participants.map((participantId) => participantId.id),
+                    participants: appointment.appointmentParticipants.map((participant) => participant.participant.id),
                 };
             });
         });
