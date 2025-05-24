96f70ea32d6c113913a3a53aedd4c9199289e2f1
Mehul <mehul.v1990@gmail.com>
2025-05-24 19:11:07 +0530

    change in html attribute
    - changed to className from class in loader component

--- src/components/ui/loader.jsx
+++ src/components/ui/loader.jsx
@@ -1,5 +1,5 @@
 const Loader = ({ loaderColor }) => {
-    return <div class={`w-4 h-4 border-2 border-t-white ${loaderColor ? loaderColor : "border-[rgba(247,237,237,0.1)]"} border-solid rounded-full animate-spin mx-auto`} />
+    return <div className={`w-4 h-4 border-2 border-t-white ${loaderColor ? loaderColor : "border-[rgba(247,237,237,0.1)]"} border-solid rounded-full animate-spin mx-auto`} />
 
 };
 
